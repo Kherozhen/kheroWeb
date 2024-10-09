ae4d2d3c3f305e86b25e83468417fd2f479d5619
Khero <kherozhen@gmail.com>
2024-10-09 10:21:25 +0200

    edit sass

--- src/react/Components/Thanks/Thanks.jsx
+++ src/react/Components/Thanks/Thanks.jsx
@@ -5,7 +5,7 @@ function Thanks() {
     return (
         <div className="thanks">
             <p>Ce site ne collecte aucune données, vous pouvez retrouver son code sur le site, 
-                <a href="https://github.com/Kherozhen/KheroWeb">GitHub</a>.
+                <span> <a href="https://github.com/Kherozhen/KheroWeb">GitHub</a></span>.
             </p>
             <p>Merci pour votre visite, à bientôt !</p>
         </div>
